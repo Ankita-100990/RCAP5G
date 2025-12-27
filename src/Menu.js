dc46c9d32b9ed2a8104cae219a91780f2fa21d88
unknown <CHIRAG@CHIRAG.focus.local>
2025-12-27 17:22:11 +0530

    Added Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -2,16 +2,32 @@
 import "./App.css";
 // import { NavLink } from "react-router-dom";
 
-
+import logo from "./Images/focus.png";
 
 
 const Menu = () => {
-  return (
-    <>
-       <div className="container-fluid bg-primary text-white">RCAP5G</div>
+    return (
+        <>
+        <div class="container-fluid">
+            <div className="box row">
+            
+                <div className="p-2 container-fluid text-white fw-bold col-6">
+                    <img src={logo} alt="" width={25} style={{ marginRight: 5 }} />
+                    RCAP5G
+                    </div>
 
-    </>
-  );
+
+                <div className="p-2 container-fluid  text-white col-6  d-flex flex-row-reverse "
+                 >
+                    <i class="material-icons " style={{marginRight :"20px"}}>account_circle</i>
+                     <i class="material-icons " style={{marginRight :"20px"}}>account_circle</i>
+                     <i class="material-icons " style={{marginRight :"20px"}}>account_circle</i>
+                     <i class="material-icons " style={{marginRight :"20px"}}>account_circle</i>
+                </div>
+            </div>
+            </div>
+        </>
+    );
 };
 
 export default Menu;
